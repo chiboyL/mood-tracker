ec7d68138affbe33511e9cd9f2ca054dde9f8d8d
chiboyL <chinonsoekezie3@gmail.com>
2025-05-01 00:10:22 -0900

    test: tested my two classes to make sure they were working properly

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import './style.css';
+import MoodEntry from './models/MoodEntry';
+import User from './models/User';
 
+const userOne = new User("Chinonso Ekezie", "https://exmaple.com");
+console.log(userOne.getUserDetails());
+
+const mood = new MoodEntry('2025-04-20', 'happy', 8, "This is really fun", "Energetic, focused");
+console.log(mood.displayEntry());
--- src/models/MoodEntry.js
+++ src/models/MoodEntry.js
@@ -1,9 +1,10 @@
 export default class MoodEntry {
-    constructor(date, mood, reflection, feelings){
+    constructor(date, mood, reflection, feelings, sleep){
         this.date = date,
         this.mood = mood,
         this.reflection = reflection,
-        this.feelings = feelings
+        this.feelings = feelings,
+        this.sleep = sleep
     }
     displayEntry(){
         return `On ${this.date}, Mood: ${this.mood}, Sleep: ${this.sleep} hrs. Reflection: ${this.reflection}, Feelings: ${this.feelings}`;
