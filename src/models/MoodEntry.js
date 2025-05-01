3398334ab350322526c7c46ceec3a42f6f9b9b9c
chiboyL <chinonsoekezie3@gmail.com>
2025-04-30 23:50:58 -0900

    feat: created a moodentry class that will hold each users entry data

---
+++ src/models/MoodEntry.js
@@ -0,0 +1,8 @@
+export default class MoodEntry {
+    constructor(date, mood, reflection, feelings){
+        this.date = date,
+        this.mood = mood,
+        this.reflection = reflection,
+        this.feelings = feelings
+    }
+}
